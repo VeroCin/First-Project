d76dda546e4fbc7eba10bec453486a2134f0da68
VeroCin <veronica_mircea@yahoo.com>
2024-10-12 20:50:53 +0200

    Update pw.js

--- pw.js
+++ pw.js
@@ -1,10 +1,10 @@
 let countClick = 0;
 let colorNewBtn = document.getElementById("buton");
 function changeColor() {
-  let colorList = ["red","yellow","green"];
-  colorNewBtn.style.backgroundColor = colorList[countClick];
-  ++countClick;
-  if (countClick === colorList.length){
-    countClick = 0;
-  }
+	let colorList = ["red","yellow","green"];
+	colorNewBtn.style.backgroundColor = colorList[countClick];
+	++countClick;
+	if (countClick === colorList.length){
+		countClick = 0;
+	}
 }
