b5f585090bc53d5abd5d346b17ca8fff9e0a07fc
VeroCin <veronica_mircea@yahoo.com>
2024-10-13 20:55:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let countClick = 0;
+let colorNewBtn = document.getElementById("buton");
+function changeColor() {
+	let colorList = ["red","yellow","green"];
+	colorNewBtn.style.backgroundColor = colorList[countClick];
+	++countClick;
+	if (countClick === colorList.length){
+		countClick = 0;
+	}
+}
