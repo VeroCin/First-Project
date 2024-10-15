a63d8cc9581f900c19d472bdc09dc5edb6592570
VeroCin <veronica_mircea@yahoo.com>
2024-10-15 22:58:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let countClick = 0;
 let colorNewBtn = document.getElementById("buton");
+
 function changeColor() {
-	let colorList = ["red","yellow","green"];
+	let colorList = ["red", "yellow", "green"];
 	colorNewBtn.style.backgroundColor = colorList[countClick];
 	++countClick;
 	if (countClick === colorList.length){
 		countClick = 0;
 	}
 }
+let myInterval = setInterval(changeColor, 10000);
